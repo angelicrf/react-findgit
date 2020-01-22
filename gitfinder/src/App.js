84683d1bfc4117298276b56f4cb250c886d69013
Your Name <angelicrf@gmail.com>
2020-01-22 10:59:24 -0800

    InitialCommit

---
+++ gitfinder/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class App extends Component{
+    render() {
+        return (
+            <Fragment className="App">
+                <header className="App-header">
+                </header>
+            </Fragment>
+        );
+    }
+
+}
+
+export default App;
