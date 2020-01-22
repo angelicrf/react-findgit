1a0c05c533a0f623988b66b3188c6c92a796643a
Your Name <angelicrf@gmail.com>
2020-01-22 11:57:24 -0800

    WNavbar

--- gitfinder/src/App.js
+++ gitfinder/src/App.js
@@ -1,14 +1,14 @@
 import React, {Component, Fragment } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/layout/Navbar";
 
 
 class App extends Component{
     render() {
         return (
             <Fragment className="App">
-                <header className="App-header">
-                </header>
+            <Navbar />
             </Fragment>
         );
     }
